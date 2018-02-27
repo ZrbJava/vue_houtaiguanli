cb0cfd4540fe36d99cc2d891d0d26a3394689906
jackie_zhao <zrbjava@gmail.com>
2018-02-27 11:32:54 +0800

    项目结构搭建

---
+++ src/component/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+      {{msg}}
+  </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            msg:" hello root components!"
+        }
+    }
+}
+</script>
+<style>
+    .box{
+        background: #333222;
+    }
+</style>
